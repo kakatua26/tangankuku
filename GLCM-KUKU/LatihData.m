c923b2208bee52f7cfaa150c2963077ac61f5db9
kakatua26 <38389332+kakatua26@users.noreply.github.com>
2018-12-11 01:27:58 +0700

    Add files via upload

---
+++ GLCM-KUKU/LatihData.m
@@ -0,0 +1,26 @@
+clc;
+D = 'D:\Danny\GLCM-KUKU\DataLatih';
+imagetrains =  dir(fullfile(D,'*.jpg'));
+nfiles=length(imagetrains);
+ciriLatih = [];
+kelasCiri = [];
+
+%Loop Data Latih
+for ii=1:nfiles
+    imagetrains(ii).name
+    gambarIni = fullfile(D,imagetrains(ii).name);
+    ini_ajah = imread(gambarIni);
+    grayImage = rgb2gray(ini_ajah);
+    if ii<100
+        kelasCiri = [kelasCiri 0];
+    else
+        kelasCiri = [kelasCiri 1];
+    end
+
+       glcm = graylevel_comat(grayImage);
+       ciriLatih = [ciriLatih; glcm];
+end
+
+csvwrite('CiriLatih.txt',ciriLatih);
+csvwrite('KelasCiri.txt',kelasCiri);
+%------------
---
+++ GLCM-KUKU/mainglcm.m
@@ -0,0 +1,29 @@
+function [hasil] = mainglcm(knn)
+    
+    fileLatih = 'CiriLatih.txt';
+    fileKelas = 'KelasCiri.txt';
+    ciriLatih = csvread(fileLatih);
+    kelasCiri = csvread(fileKelas);
+    kelasCiri = kelasCiri(:);
+    D1 = 'D:\Danny\GLCM-KUKU\DataUji'; %image dari android disimpan disini
+    imagetest = dir(fullfile(D1,'gambar.jpg'));
+    uji = imread(fullfile(D1,imagetest.name));
+    
+    
+    %ubah K disini
+    k = knn;
+
+    %Training
+    train = fitcknn(ciriLatih,kelasCiri,'NumNeighbors',k,'Standardize',1);
+    
+    dataUji = rgb2gray(uji);
+    glcmUji = graylevel_comat(dataUji);
+    %Predict
+    [label,score,cost] = predict(train,glcmUji);
+
+    if label == 0
+        hasil = 'SEHAT';
+    else
+        hasil = 'TERRYS';
+    end
+end
