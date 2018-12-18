05153dbd00b010af1d4816dca2fa5caa67d96d69
kakatua26 <38389332+kakatua26@users.noreply.github.com>
2018-12-18 21:25:53 +0700

    Update graylevel_comat.m

--- GLCM-KUKU/graylevel_comat.m
+++ GLCM-KUKU/graylevel_comat.m
@@ -1,24 +1,35 @@
 function [hasilanalisis] = graylevel_comat(input)
 
 %     gray level (skala 1-8)
-    range = getrangefromclass(input);
-    slope = 8 / (range(2)-range(1));
-    intercept = 1 - (slope*(range(1)));
-    input = floor(imlincomb(slope,input,intercept,'double'));
-    input(input > 8) = 8;
-    input(input < 1) = 1;
-    
+%     range = getrangefromclass(input);
+%     slope = 8 / (range(2)-range(1));
+%     intercept = 1 - (slope*(range(1)));
+%     input = floor(imlincomb(slope,input,intercept,'double'));
+%     input(input > 8) = 8;
+%     input(input < 1) = 1;
+      
 %     glcm, analisis secara horizontal
-    output = zeros(8,8);
-    for i=1:size(input,1)
-        for j=1:size(input,2)-1
-            output(input(i,j), input(i,j+1)) = output(input(i,j), input(i,j+1))+1;
-        end
-    end
-    
-%     normalisasi
-    glcm = output/sum(sum(output));
+%     output = zeros(8,8);
+%     for i=1:size(input,1)
+%         for j=1:size(input,2)-1
+%             output(input(i,j), input(i,j+1)) = output(input(i,j), input(i,j+1))+1;
+%         end
+%     end
 
+%     gray level skala 1-4
+      input(input <= 63) = 0;
+      input(input <= 127 & input >= 64) = 1;
+      input(input <= 128 & input >= 191) = 2;
+      input(input <= 192 & input >= 255) = 3;
+      
+      %kookurens
+      [glcm, SI] = graycomatrix(input,'NumLevels',4,'GrayLimits',[]);
+      %penjumlahan asli dan transpos
+      glcm2 = [glcm] + [glcm]';
+      
+%     normalisasi
+%    glcm = output/sum(sum(output));
+      glcm = glcm2/sum(sum(glcm2));
 %     tekstur
     energi = sum(glcm(:).^2);
     entrophy = sum(glcm(:).*mylog2(glcm(:)));
