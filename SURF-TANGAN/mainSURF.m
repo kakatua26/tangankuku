a6ad5c6614088cd106b5f94f2d6bab3c2babd20c
kakatua26 <38389332+kakatua26@users.noreply.github.com>
2018-12-20 18:34:47 +0700

    Update mainSURF.m

--- SURF-TANGAN/mainSURF.m
+++ SURF-TANGAN/mainSURF.m
@@ -1,8 +1,8 @@
 function hasil = mainSURF(knn)
-    D1 = 'D:\Danny\SURF-TANGAN\dataUji'; %image dari android disimpan disini
+    D1 = 'D:\SURF-TANGAN\DataUji'; %image dari android disimpan disini
     imagetest = dir(fullfile(D1,'gambar.jpg'));
     uji = imread(fullfile(D1,imagetest.name));
-    k = knn;
+    k = 5;
     
     fileLatih = 'ciriLatih.txt';
     fileKelas = 'kelasCiri.txt';
@@ -17,21 +17,24 @@ function hasil = mainSURF(knn)
     dataUji = rgb2gray(dataUji);
     % Get the Key Points
     Options.upright=true;
-    Options.tresh=0.0001;
+    Options.tresh=0.0009;
     SurfExtract = ExtractSURF(dataUji,Options);
     FeatureVec = reshape([SurfExtract.descriptor],64,[]);
-    klaster = kmeans(FeatureVec,3,'Distance','cityblock');
+    rng(1)
+    klaster = kmeans(FeatureVec,25,'Distance','cityblock');
     panjang = length(klaster);
     Testfeatures = reshape(klaster, [1 panjang]);
     % tempUji = detectSURFFeatures(dataUji);
     % [im_features, temp] = extractFeatures(dataUji, temp);
     % features = im_features(:);
     % panjang = length(features);
+    
     [label,score,cost] = predict(train,Testfeatures);
+    
     % 
     if label == 0
         hasil = 'HFMD';
     else
-        hasil = 'SEHAT';
+        hasil = 'BUKAN HFMD';
     end
 end
