77c0bef3111b49987581c2fa6d89cbeaf086a970
kakatua26 <38389332+kakatua26@users.noreply.github.com>
2018-12-23 16:22:31 +0700

    Update mainglcm.m

--- GLCM-KUKU/mainglcm.m
+++ GLCM-KUKU/mainglcm.m
@@ -1,29 +1,27 @@
 function [hasil] = mainglcm(knn)
-    
+   
     fileLatih = 'CiriLatih.txt';
     fileKelas = 'KelasCiri.txt';
     ciriLatih = csvread(fileLatih);
     kelasCiri = csvread(fileKelas);
     kelasCiri = kelasCiri(:);
-    D1 = 'D:\Danny\GLCM-KUKU\DataUji'; %image dari android disimpan disini
-    imagetest = dir(fullfile(D1,'gambar.jpg'));
+    D1 = 'E:\GLCM-KUKU\DataUji\b'; %image dari android disimpan disini
+    imagetest = dir(fullfile(D1,'gambar.jpeg'));
     uji = imread(fullfile(D1,imagetest.name));
-    
-    
+      
     %ubah K disini
-    k = knn;
+    k = 1;
 
     %Training
     train = fitcknn(ciriLatih,kelasCiri,'NumNeighbors',k,'Standardize',1);
-    
+    ukuran = imresize(uji, [224 224]);
     dataUji = rgb2gray(uji);
     glcmUji = graylevel_comat(dataUji);
     %Predict
     [label,score,cost] = predict(train,glcmUji);
-
-    if label == 0
-        hasil = 'SEHAT';
-    else
-        hasil = 'TERRYS';
+    
+    if label == 1
+        hasil = 'Terrys';
+      else
+        hasil = 'Sehat';
     end
-end
